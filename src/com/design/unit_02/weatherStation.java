d6f2f8f4380c53c7c8ba8d0d22a4817978caadea
kiela929 <kiela929@naver.com>
2016-03-05 20:49:17 +0900

    옵저버 패턴 (Observer pattern)-(4) 메인함수 생성함 주제객체와 옵저버들을 생성하고, 데이터의 변화를 주는 곳!!

---
+++ src/com/design/unit_02/weatherStation.java
@@ -0,0 +1,21 @@
+package com.design.unit_02;
+
+public class weatherStation {
+	
+	public static void main(String[] args) {
+		
+		WeatherData weatherData = new WeatherData();//주제객체생성
+		
+		CurrentConditionsDisplay conditionsDisplay=
+				new CurrentConditionsDisplay(weatherData);
+		//현재 조건을 표시하는 디스플레이임
+		
+		TotalDisplay totalDisplay=new TotalDisplay(weatherData);
+		//기상통계 표시하는 디스플레이임
+		
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+
+}
