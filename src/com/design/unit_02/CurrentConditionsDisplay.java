4f1ca6ce25e68a2f0d82171e1bbb8bec352203bd
kiela929 <kiela929@naver.com>
2016-03-05 20:48:02 +0900

    옵저버 패턴 (Observer pattern)-(3) 옵저버 생성함 (디스플레이 기능을 가짐) 주제객체가 변경된 사항을 전달하면 그 정보를 이용하여 원하는 정보로 가공 후 화면에 보여주는 역할을 함. 옵저버는 여러개 존재 가능

---
+++ src/com/design/unit_02/CurrentConditionsDisplay.java
@@ -0,0 +1,49 @@
+package com.design.unit_02;
+
+/*
+ * 디스플레이 객체 - DisplayElement,Observer 인터페이스 구현함
+ * 
+ * 현재 만들어야하는 디스플레이는
+ * 1) 현재 조건을 표시하는 디스플레이
+ * 2) 기상통계를 표시하는 디스플레이
+ * 
+ * 현재 객체는 1)번 현재조건을 표시하는 디스플레이를 구현한것.
+ * */
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	 //weatherData 객체로 부터 변경사항을 받기 위해 obserber를 구현한다.
+	//DisplayElement로 화면을 보이게 하기로 약속했기 때문에 구현함.
+	
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+
+	
+	public CurrentConditionsDisplay(Subject weatherData) {
+		// 생성자에 weatherData 라는 주제객체가 전달되며, 그 객체를 써서 디스플레이를 옵저버로 등록한다.
+		
+		this.weatherData = weatherData; 
+		weatherData.registerObserver(this);
+		//현재는 옵저버이므로 주제객체인 weatherData에서 옵저버 등록을 해야한다. 
+	}
+	
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		
+		this.temperature = temperature;
+		this.humidity=humidity;
+		this.display();
+
+	}
+	
+	
+	@Override
+	public void display() { //화면출력
+		System.out.println("< 현재조건을 표시하는 디스플레이 >");
+		System.out.println("Current conditions : "+temperature
+				+"F degrees and "+humidity+ "% humidity");
+
+	}
+
+
+
+}
---
+++ src/com/design/unit_02/TotalDisplay.java
@@ -0,0 +1,44 @@
+package com.design.unit_02;
+
+/*
+ * 디스플레이 객체 - DisplayElement,Observer 인터페이스 구현함
+ * 
+ * 현재 만들어야하는 디스플레이는
+ * 1) 현재 조건을 표시하는 디스플레이
+ * 2) 기상통계를 표시하는 디스플레이
+ * 
+ * 현재 객체는 2)번 기상통계를 표시하는 디스플레이를 구현한것.
+ * */
+
+public class TotalDisplay implements Observer, DisplayElement{
+	
+	private Subject weatherData;
+	private float totalTemperature;
+	private float totalHumidity;
+	private int count; //평균을 내기 위해 count변수 둠 
+
+	
+	public TotalDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+		count=1; //생성할때 1로 초기화
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		this.totalTemperature+=temperature;
+		this.totalHumidity+= humidity;
+		this.display();
+		this.count++;
+		
+	}
+
+	@Override
+	public void display() {
+		System.out.println("< 기상통계 표시하는 디스플레이 >");
+		System.out.println((totalTemperature/count)+"도, "+(totalHumidity/count)+"% 이다.");
+	}
+	
+	
+
+}
